9a7263b8ad144c8c81969256c7959c3aac7551c9
hasant23 <h.tuten23@gmail.com>
2022-04-06 01:23:25 +0300

    new

---
+++ hands-on/Terraform/terraform-02_Terraform_Commands_Varables_Conditonals_Loops/terraform-aws/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.69.0"
+    }
+  }
+}
+
+resource "aws_instance" "tf-ec2" {
+  ami           = "ami-0ed9277fb7eb570c9"
+  instance_type = "t2.micro"
+  key_name      = "hasan"    # write your pem file without .pem extension>
+  tags = {
+    "Name" = "tf-ec2"
+  }
+}
+
+resource "aws_s3_bucket" "tf-s3" {
+  bucket = "hasan-tf-test-bucket-addwhateveryouwant"
+  acl    = "private"
+}
