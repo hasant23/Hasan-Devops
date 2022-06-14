fd66c81a5fc464bb245cb2cbea4869b17e56ba50
hasant23 <h.tuten23@gmail.com>
2022-06-14 20:05:11 +0300

    new

--- hands-on/ECR/ecr-02a-jenkins-pipeline-to-deploy-docker-images-on-ECR-to-ECS-cluster/ecr-ecs-ec2-jenkins.tf
+++ hands-on/ECR/ecr-02a-jenkins-pipeline-to-deploy-docker-images-on-ECR-to-ECS-cluster/ecr-ecs-ec2-jenkins.tf
@@ -18,11 +18,11 @@ provider "github" {
 data "aws_caller_identity" "current" {}
 
 locals {
-  github-email = "serdar@clarusway.com"                      # you need to change this line
-  github-username = "serdarcw"                              # you need to change this line
+  github-email = "h.tuten23@gmail.com"                      # you need to change this line
+  github-username = "hasant23"                              # you need to change this line
   github-token = ""      # you need to change this line
   key_pair=""            # you need to change this line
-  pem_key_address = "~/Downloads/serdar.pem"              # you need to change this line
+  pem_key_address = "~/Downloads/hasan.pem"              # you need to change this line
 }
 
 resource "github_repository" "githubrepo" {
